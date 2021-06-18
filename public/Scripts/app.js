772fbd792bc1cdc940876d657fa04d1471d4a914
etedali <etedali@gmail.com>
2021-06-17 20:18:35 -0400

    fix cancel button in warning alert

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,21 +1,21 @@
 /*  File name: app.js
 	  Author's name: Mohammad Etedali - 301056465
 	  Website address: https://comp299.herokuapp.com
-      Date: 5/30/21
+      Date: 5/17/21
 	  Description: This file for custome js  */
 
 
 (function(){
     function start(){
         console.log('App started...')
-
+        //Find all button that has danger class 
         let deleteButtons = document.querySelectorAll('.btn-danger');
-
+        //in this for add event for the delete button 
         for(button of deleteButtons){
             button.addEventListener('click',(event) => {
                 if(!confirm('Are you sure?')){
                     event.preventDefault();
-                    window.location.assign('/book-list');
+                    window.location.assign('/contact-list');
                 }
             })
         }
